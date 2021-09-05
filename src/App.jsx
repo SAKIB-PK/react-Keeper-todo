77fa7e534eaad25dc615bb32afc9737be6108e94
Mohammad Ali Sakib <mohammad.ali.sakib.pk@gmail.com>
2021-09-06 01:24:52 +0600

    initial ui design

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Footer from './Component/Footer'
+import Header from './Component/Header'
+import Note from './Component/Note'
+
+function App() {
+    return (
+        <div>
+            <Header />
+            <Note/>
+            <Footer/>
+        </div>
+    )
+}
+
+export default App
